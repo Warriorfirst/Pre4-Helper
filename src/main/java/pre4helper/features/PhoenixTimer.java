56c6227e4ad18cb93f44a8f08ab29ea6a69ce312
Warriorfirst <84595526+Warriorfirst@users.noreply.github.com>
2024-01-14 03:08:32 +0100

    Slider update

--- src/main/java/pre4helper/features/PhoenixTimer.java
+++ src/main/java/pre4helper/features/PhoenixTimer.java
@@ -15,9 +15,9 @@ public class PhoenixTimer{
     public void onChat(ClientChatReceivedEvent event) {
         String message = event.message.getUnformattedText();
         
-        if(Pre4Config.phoenixTimer && message.contains("Your Phoenix Pet saved you from certain death!")) {
+        if(message.contains("Your Phoenix Pet saved you from certain death!")) {
             drawTitle("", null, 20);
-            Utils.setTimeout(() -> Minecraft.getMinecraft().ingameGUI.displayTitle("Leap now!", null, 0, 20, 0), 3750);
+            Utils.setTimeout(() -> Minecraft.getMinecraft().ingameGUI.displayTitle("Leap now!", null, 0, 20, 0), Pre4Config.phoenixTimer);
         }
 
     }
